fa3bb942f302b277f5c9ec8a89745c4bdb71c2de
Loenix <florent@sowapps.com>
2021-03-23 14:25:49 +0100

    Generate new doc of v3.8.0 Remove old eclipse files New phpdoc script Update phpDocumentor to v3

--- README.md
+++ README.md
@@ -1,5 +1,4 @@
 # orpheus-doc
 Documentation of Orpheus Framework
 
-To generate documentation, open a terminal and inside a the doc project, run
-php phpdoc.php
+To generate documentation, open a terminal and inside the doc project, run php phpdoc.php
--- phpdoc.php
+++ phpdoc.php
@@ -1,3 +1,4 @@
+#!/usr/bin/env php
 <?php
 
 /*
@@ -8,4 +9,34 @@
  * php phpdoc.php -d vendor/orpheus/ -t html/
  */
 
-require_once 'vendor/phpdocumentor/phpdocumentor/bin/phpdoc';
+$phpDocumentorVersion = 'v3.0.0';
+$phpDocumentorDownloadUrl = sprintf('https://github.com/phpDocumentor/phpDocumentor/releases/download/%s/phpDocumentor.phar', $phpDocumentorVersion);
+$phpDocumentorFile = 'phpDocumentor.phar';
+$docSource = 'vendor/orpheus/';
+$docTarget = 'html/';
+$docCache = 'cache/';
+
+function writeInfo($text) {
+	echo "\e[34m" . $text . "\e[0m" . PHP_EOL;
+}
+
+function writeSuccess($text) {
+	echo "\e[32m" . $text . "\e[0m" . PHP_EOL . PHP_EOL;
+}
+
+# Change working directory to this file's folder
+chdir(__DIR__);
+
+if( !file_exists($phpDocumentorFile) ) {
+	writeInfo(sprintf('Downloading new %s...', $phpDocumentorFile));
+	file_put_contents($phpDocumentorFile, fopen($phpDocumentorDownloadUrl, 'r'));
+	writeSuccess('File downloaded');
+}
+
+writeInfo('Updating composer...');
+passthru('composer update');
+writeSuccess('Composer up-to-date');
+
+writeInfo('Generating new documentation...');
+passthru(sprintf('php phpDocumentor.phar run -d %s -t %s --cache-folder %s', $docSource, $docTarget, $docCache));
+writeSuccess('Documentation generated');
